3e8830a2e9dbb4644ff51e447aba4a1f60c39b1d
Aidan Walsh <ajw137@pitt.edu>
2021-03-05 00:41:45 -0500

    first commit

---
+++ Assets/MenuController.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+public var helpMenu : GameObject;
+function Start () {
+	
+}
+
+function Update () {
+	
+}
+public function help(){
+	helpMenu.SetActive(true);
+
+}
+
+public function disableHelp(){
+	helpMenu.SetActive(false);
+
+}
---
+++ Assets/Ticker.js
@@ -0,0 +1,54 @@
+﻿#pragma strict
+import UnityEngine.UI;
+public var text : Text;
+public var sound : AudioClip;
+public var resetSound : AudioClip;
+
+private var audio1 : AudioSource;
+private var money : float;
+function Start () {
+	audio1 = this.GetComponent(typeof AudioSource);	
+}
+
+function Update () {
+	if(Input.GetKeyDown(KeyCode.Q)){
+		money += 0.25f;
+		UpdateText();
+	}
+	if(Input.GetKeyDown(KeyCode.N)){
+		money += 0.05f;
+		UpdateText();
+	}
+	if(Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.LeftShift)){
+		money += 0.01f;
+		UpdateText();
+	}
+	if(Input.GetKeyDown(KeyCode.D)){
+		money += 0.10f;
+		UpdateText();
+	}
+	if(Input.GetKeyDown(KeyCode.H)){
+		money += 0.50f;
+		UpdateText();
+	}
+}
+function UpdateText(){
+	money = (Mathf.Round(money * 100)/100);
+	text.text = "$"+money.ToString();
+	audio1.PlayOneShot(sound);
+	//text.text = "$"+money.toFixed(2);
+}
+
+function AddMoney(amount : float){
+	money += amount;
+	UpdateText();
+
+}
+
+function Reset(){
+	money = 0.0f;
+	text.text = "$0.00";
+	audio1.PlayOneShot(resetSound);
+	//UpdateText();
+
+}
